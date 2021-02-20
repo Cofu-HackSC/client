566bb7e68dbabc66e71517040925fdd6c0d2c09f
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-19 23:41:04 -0800

    updated feed tiles

--- app/lib/components/feed_tile.dart
+++ app/lib/components/feed_tile.dart
@@ -1,4 +1,5 @@
 import 'package:app/components/themed_text.dart';
+import 'package:app/global/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class FeedTile extends StatelessWidget {
@@ -7,6 +8,7 @@ class FeedTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(22.0),
       child: Card(
+        color: AppTheme.backgroundLighGray,
         elevation: 0,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
@@ -14,36 +16,62 @@ class FeedTile extends StatelessWidget {
         child: ClipRRect(
           borderRadius: BorderRadius.circular(16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.asset(
                 'assets/example_img.jpg',
                 fit: BoxFit.cover,
-                height: 142,
+                height: 180,
                 width: MediaQuery.of(context).size.width,
               ),
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     ThemedText(
                       'Homemade Pretzels',
                       type: Type.h2,
                     ),
-                    ThemedText(
-                      '\$1.50',
-                      type: Type.subtitle,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ThemedText(
+                          'Delivery and Pickup',
+                          type: Type.subtitle,
+                        ),
+                        Stars(4.2),
+                      ],
                     ),
                   ],
                 ),
-              ),
-              Divider(),
-              ThemedText('Delivery and Pickup')
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class Stars extends StatelessWidget {
+  final double stars;
+  Stars(this.stars);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ThemedText(
+          stars.toString(),
+          type: Type.subtitle,
+        ),
+        Icon(
+          Icons.star_rounded,
+          size: 16,
+          color: AppTheme.star,
+        ),
+      ],
     );
   }
 }
