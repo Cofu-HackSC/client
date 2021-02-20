3bfad1f6b90f9d176880d04d6e3f6b47f72d5a5b
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-20 14:03:20 -0800

    updated tab bar colors

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:app/components/button.dart';
 import 'package:app/components/header.dart';
 import 'package:app/models/cook_profile.dart';
+import 'package:app/global/app_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:app/pages/settings.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
+          activeColor: AppTheme.primary,
           items: [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
