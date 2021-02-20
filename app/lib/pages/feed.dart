82762fd93ff15d5270723e9a196cdaf1175156c2
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-19 22:44:32 -0800

    worked on feed

---
+++ app/lib/pages/feed.dart
@@ -0,0 +1,25 @@
+import 'package:app/components/header.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatefulWidget {
+  @override
+  _FeedPageState createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        Header('Feed'),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: CupertinoSearchTextField(),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -2,6 +2,8 @@ import 'package:app/components/button.dart';
 import 'package:app/components/header.dart';
 import 'package:flutter/material.dart';
 
+import 'feed.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -17,11 +19,8 @@ class _HomePageState extends State<HomePage> {
 
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
+  static List<Widget> _pages = <Widget>[
+    FeedPage(),
     Text(
       'Index 1: Business',
       style: optionStyle,
@@ -48,30 +47,20 @@ class _HomePageState extends State<HomePage> {
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.shopping_bag),
+            label: 'Orders',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
       ),
-      body: CustomScrollView(
-        slivers: [
-          Header('Home Page'),
-          SliverFillRemaining(
-            child: Center(
-              child: Button(
-                'Log Out',
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-          ),
-        ],
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
       ),
     );
   }
