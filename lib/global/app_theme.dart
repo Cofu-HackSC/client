da023630db49ee2b24f2de5877c0c418efd3adad
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-20 11:32:09 -0800

    changed primary color

--- lib/global/app_theme.dart
+++ lib/global/app_theme.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.green;
+  static const Color primary = Colors.black;
   static const Color primaryText = Color(0xff000000);
   static const Color secondaryText = Color(0xff909090);
   static const Color buttonText = Color(0xffffffff);
