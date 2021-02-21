ccd4755d601ebceaefc8f03005639b0d85a29593
jasonTelanoff <jason.telanoff@gmail.com>
2021-02-20 17:33:28 -0800

    yes

--- lib/pages/application.dart
+++ lib/pages/application.dart
@@ -41,59 +41,8 @@ class _ApplicationState extends State<Application> {
             delegate: SliverChildListDelegate(
               [
                 ThemedText('License'),
-                DocumentTile(),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Button('Click here to upload your license',
-                      onPressed: () {
-                    print(widget.session.session);
-                    try {
-                      print('Started Picture');
-                      ImagePicker()
-                          .getImage(source: ImageSource.camera)
-                          .then((value) async {
-                        print('Finished Picture');
-
-                        MultipartRequest req = MultipartRequest(
-                          'POST',
-                          Uri.parse(
-                            'https://cofu-305406.wl.r.appspot.com/application',
-                          ),
-                        );
-                        print('here 1');
-                        req.headers['Cookie'] =
-                            'connect.sid=' + widget.session.session;
-                        print('here 2');
-
-                        req.files.add(
-                          MultipartFile(
-                            'file',
-                            File(value.path).readAsBytes().asStream(),
-                            File(value.path).lengthSync(),
-                            filename: value.path.split("/").last,
-                          ),
-                        );
-                        print('here 3');
-                        var res = await req.send();
-
-                        print('RESPONSE');
-                        print(res.statusCode);
-                        print(res.reasonPhrase);
-
-                        await res.stream
-                            .drain()
-                            .then(
-                                (value) => print("VALUEEE " + value.toString()))
-                            .then(
-                              (value) => Navigator.pop(context),
-                            );
-                      });
-                    } catch (e) {
-                      print('ERROROROROROORORORO');
-                      print(e);
-                    }
-                  }),
-                )
+                DocumentTile('Proof of Insurance', widget.session),
+                DocumentTile('Cottage Food License', widget.session),
               ],
             ),
           ),
@@ -104,6 +53,10 @@ class _ApplicationState extends State<Application> {
 }
 
 class DocumentTile extends StatefulWidget {
+  final Session session;
+  final String name;
+  DocumentTile(this.name, this.session);
+
   @override
   _DocumentTileState createState() => _DocumentTileState();
 }
@@ -113,8 +66,8 @@ class _DocumentTileState extends State<DocumentTile> {
 
   @override
   void initState() {
-    hasImage = true;
-    blurred = false;
+    hasImage = false;
+    blurred = true;
     super.initState();
   }
 
@@ -123,55 +76,107 @@ class _DocumentTileState extends State<DocumentTile> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Column(
-            children: [
-              TextButton(
-                style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                onPressed: () {
-                  if (hasImage) setState(() => blurred = !blurred);
-                },
-                child: hasImage
-                    ? blurred
-                        ? ClipRect(
-                            child: Container(
+          borderRadius: BorderRadius.circular(12),
+          child: Container(
+            color: AppTheme.backgroundLighGray,
+            child: Column(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                  onPressed: () {
+                    if (hasImage)
+                      setState(() => blurred = !blurred);
+                    else {
+                      try {
+                        print('Started Picture');
+                        ImagePicker()
+                            .getImage(source: ImageSource.camera)
+                            .then((value) async {
+                          print('Finished Picture');
+
+                          MultipartRequest req = MultipartRequest(
+                            'POST',
+                            Uri.parse(
+                              'https://cofu-305406.wl.r.appspot.com/application',
+                            ),
+                          );
+                          print('here 1');
+                          req.headers['Cookie'] =
+                              'connect.sid=' + widget.session.session;
+                          print('here 2');
+
+                          req.files.add(
+                            MultipartFile(
+                              'file',
+                              File(value.path).readAsBytes().asStream(),
+                              File(value.path).lengthSync(),
+                              filename: value.path.split("/").last,
+                            ),
+                          );
+                          print('here 3');
+                          var res = await req.send();
+
+                          print('RESPONSE');
+                          print(res.statusCode);
+                          print(res.reasonPhrase);
+
+                          await res.stream
+                              .drain()
+                              .then((value) =>
+                                  print("VALUEEE " + value.toString()))
+                              .then(
+                                (value) => Navigator.pop(context),
+                              );
+                        });
+                      } catch (e) {
+                        print('ERROROROROROORORORO');
+                        print(e);
+                      }
+                    }
+                  },
+                  child: hasImage
+                      ? blurred
+                          ? ClipRect(
+                              child: Container(
+                                height: 180,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('assets/example_img.jpg'),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                child: BackdropFilter(
+                                  filter:
+                                      ImageFilter.blur(sigmaX: 10, sigmaY: 8),
+                                  child: Container(
+                                    color: Colors.black.withOpacity(0.1),
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Image.asset('assets/example_img.jpg',
                               height: 180,
                               width: double.infinity,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('assets/example_img.jpg'),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 8),
-                                child: Container(
-                                  color: Colors.black.withOpacity(0.1),
-                                ),
-                              ),
+                              fit: BoxFit.cover)
+                      : Container(
+                          height: 180,
+                          width: double.infinity,
+                          color: AppTheme.logoGreen.withAlpha(100),
+                          child: Center(
+                            child: Icon(
+                              Icons.add,
+                              color: AppTheme.primaryText,
                             ),
-                          )
-                        : Image.asset('assets/example_img.jpg',
-                            height: 180,
-                            width: double.infinity,
-                            fit: BoxFit.cover)
-                    : Container(
-                        height: 180,
-                        width: double.infinity,
-                        color: AppTheme.logoGreen.withAlpha(100),
-                        child: Center(
-                          child: Icon(
-                            Icons.add,
-                            color: AppTheme.primaryText,
                           ),
                         ),
-                      ),
-              ),
-              Row(
-                children: [ThemedText('Proof Of Insurance')],
-              ),
-              SizedBox(height: 8)
-            ],
+                ),
+                Row(
+                  children: [ThemedText(widget.name)],
+                ),
+                SizedBox(height: 8)
+              ],
+            ),
           )),
     );
   }
